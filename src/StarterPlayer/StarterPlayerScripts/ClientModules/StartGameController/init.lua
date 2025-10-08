3d4383a17faa1c7522cbe0720f7d52edc9cdb528
Alysson Chicó <cyberroomstudios@gmail.com>
2025-10-08 01:08:40 -0300

    StartGame

--- src/StarterPlayer/StarterPlayerScripts/ClientModules/StartGameController/init.lua
+++ src/StarterPlayer/StarterPlayerScripts/ClientModules/StartGameController/init.lua
@@ -12,6 +12,16 @@ local statusIdentifier = BridgeNet2.ReferenceIdentifier("status")
 local messageIdentifier = BridgeNet2.ReferenceIdentifier("message")
 -- End Bridg Net
 
+-- Init Bridg Net
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
+local Utility = ReplicatedStorage.Utility
+local BridgeNet2 = require(Utility.BridgeNet2)
+local bridge = BridgeNet2.ReferenceBridge('ColocaAquiONomeDaBridge')
+local actionIdentifier = BridgeNet2.ReferenceIdentifier('action')
+local statusIdentifier = BridgeNet2.ReferenceIdentifier('status')
+local messageIdentifier = BridgeNet2.ReferenceIdentifier('message')
+-- End Bridg Net
+
 function StartGameController:Init(data)
 	local result = bridge:InvokeServerAsync({
 		[actionIdentifier] = "Start",
